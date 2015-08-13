40f686bba34f4ea09a69ffdba9e8cfdadab4d8c2
Ian Polding <ipolding@gmail.com>
2015-08-13 23:33:00 +0100

    Expand the map

--- JavaScript/react/google-maps-components/GoogleMapFinal.js
+++ JavaScript/react/google-maps-components/GoogleMapFinal.js
@@ -1,11 +1,44 @@
 var GoogleMap = React.createClass({
 
+    map : null,
+
+    addMarker : function(place) {
+        var marker =
+            new google.maps.Marker({
+                map: this.map,
+                anchorPoint: new google.maps.Point(0, -29)
+            });
+
+             
+
+        marker.setIcon(({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+            
+    },
+
+    placeSearchCallback : function(placeResultArray, placesServiceStatus, placeSearchPagination) {
+            this.addMarker(placeResultArray[0])
+  },
+
+    addPlace : function(address) {
+        var placesService = new google.maps.places.PlacesService(map);
+        var request = {query:address}    
+          placesService.textSearch(request, this.placeSearchCallback)
+    },
+
     componentDidMount :  function() {
         console.log("component has mounted")
-        new google.maps.Map(React.findDOMNode(this.refs.map_canvas), {
+        map = new google.maps.Map(React.findDOMNode(this.refs.map_canvas), {
             zoom: 8,
-            center: {lat: -34.397, lng: 150.644}
-        });
+            center: {lat: 51.50417, lng: 0}        });
+           
+
+        this.addPlace("Local Government Association, Smith Square, City of London, London, United Kingdom");
     },
 
     render: function() {
