734f59fb51c3b881eadb97b5619edac1949f2a1b
ian_polding <ian.polding@wowcher.co.uk>
2015-09-17 18:18:27 +0100

    Add and remove seem to be wokring. Now need to add iterator

--- Java/Coursera/deque/src/main/java/Deque.java
+++ Java/Coursera/deque/src/main/java/Deque.java
@@ -1,9 +1,10 @@
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Deque<Item> {
+public class Deque<Item> implements Iterable<Item> {
 
     public Node sentinelNode;
-    private int size;
+    private int size = 0;
 
     public Deque() {
     // initialise the sentinel node and attach to itself
@@ -16,23 +17,6 @@ public class Deque<Item> {
         return size;
     }
 
-    @Override
-    public String toString() {
-        return "Deque{" +
-                "first=" + first() +
-                ", last=" + last() +
-                ", size=" + size +
-                '}';
-    }
-
-    public Node first() {
-        return sentinelNode.next;
-    }
-
-    public Node last() {
-        return sentinelNode.previous;
-    }
-
     public void addFirst(Item item) {
         Node oldFirst = sentinelNode.next;
 
@@ -42,26 +26,57 @@ public class Deque<Item> {
         sentinelNode.next = newNode;      //3) attach the sentinel to the node
         oldFirst.previous = newNode;        //6) attach previous first to first
         size++;
-
     }
+
+    public void addLast(Item item) {
+        Node oldLast = sentinelNode.previous;
+
+        Node newNode = new Node(item);    //1) create a new nodew
+        newNode.next = sentinelNode;  //2) attach the node to the sentinel
+        newNode.previous = oldLast;
+        sentinelNode.previous = newNode;      //3) attach the sentinel to the node
+        oldLast.next = newNode;        //6) attach next Last to Last
+        size++;
+    }
+
 
     public Item removeFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException("The deque is empty!");
         }
         Node returnNode = sentinelNode.next;
-        sentinelNode.next = returnNode.next;
+        Node newFirst = returnNode.next;
+        sentinelNode.next = newFirst;
+        newFirst.previous = sentinelNode;
+        size--;
+        return (Item) returnNode.value;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("The deque is empty!");
+        }
+        Node returnNode = sentinelNode.previous;
+        Node newLast = returnNode.previous;
+        sentinelNode.previous = newLast;
+        newLast.next = sentinelNode;
+        size--;
         return (Item) returnNode.value;
     }
 
     public boolean isEmpty() {
-        return sentinelNode.next ==null && sentinelNode.previous == null;
+        return sentinelNode.next == sentinelNode && sentinelNode.previous == sentinelNode;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new DequeIterator<Item>(this);
     }
 
     public class Node<Item> {
-        public Node next;
-        public Node previous;
-        public Item value;
+        Node next;
+        Node previous;
+        Item value;
 
         public Node(Item value) {
             this.value = value;
@@ -79,6 +94,31 @@ public class Deque<Item> {
             this.value = value;
         }
 
+    }
+
+
+    private class DequeIterator<Item> implements Iterator<Item> {
+
+        private Deque<Item> deque;
+
+        public DequeIterator (Deque<Item> deque) {
+            this.deque = deque;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return deque.isEmpty();
+        }
+
+        @Override
+        public Item next() {
+            return deque.removeFirst();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
 }
--- Java/Coursera/deque/src/test/java/DequeTests.java
+++ Java/Coursera/deque/src/test/java/DequeTests.java
@@ -1,33 +1,111 @@
 import org.junit.Test;
 
-import java.util.NoSuchElementException;
-
 import static org.junit.Assert.*;
 
 public class DequeTests {
 
 
     @Test
-    public void test_shouldBeAbleToAddFirst() {
+    public void test_shouldBeAbleToAddFirst_thenRemoveFirstInReverseOrder() {
         Deque deque = new Deque();
         deque.addFirst(1);
         deque.addFirst(2);
         deque.addFirst(3);
-        deque.addFirst(4);
-        deque.addFirst(5);
-        deque.addFirst(6);
-        deque.addFirst(7);
 
-        System.out.println("Head of deque is " + deque.first().value);
-        System.out.println("Next in deque is " + deque.first().next.value);
-        System.out.println("Next in deque comes after = " + deque.first().next.previous.value);
-        System.out.println("Next in deque comes before = " + deque.first().next.next.value);
-        System.out.println(deque.first().previous.value + "::" + deque.first().value + "::" + deque.first().next.value + "::" + deque.first().next.next.value);
-
-        assertEquals(7, deque.size());
-
-        System.out.println("Sentinel node's previous is = " + deque.sentinelNode.previous.value);
+        assertEquals(3, deque.removeFirst());
+        assertEquals(2, deque.removeFirst());
+        assertEquals(1, deque.removeFirst());
     }
 
+    @Test
+    public void test_shouldStartEmpty() {
+        assertTrue(new Deque<Integer>().isEmpty());
+    }
+
+    @Test
+    public void removeFirstShouldEmptyDeque() {
+
+        Deque deque = new Deque();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+
+        assertEquals(3, deque.removeFirst());
+        assertEquals(2, deque.removeFirst());
+        assertEquals(1, deque.removeFirst());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void removeLastShouldWorkWithCallsToAddFirst() {
+
+        Deque deque = new Deque();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+
+        assertEquals(1, deque.removeLast());
+        assertEquals(2, deque.removeLast());
+        assertEquals(3, deque.removeLast());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void test_shouldBeAbleToAddLast_thenRemoveLastInReverseOrder() {
+        Deque deque = new Deque();
+        deque.addLast(1);
+            assertEquals(1, deque.size());
+        deque.addLast(2);
+            assertEquals(2, deque.size());
+        deque.addLast(3);
+            assertEquals(3, deque.size());
+
+        assertEquals(3, deque.removeLast());
+        assertEquals(2, deque.removeLast());
+            assertEquals(1, deque.size());
+        assertEquals(1, deque.removeLast());
+            assertEquals(0, deque.size());
+    }
+
+    @Test
+    public void removeFirstShouldWorkWithCallsToAddLast() {
+
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+
+        assertEquals(1, deque.removeFirst());
+        assertEquals(2, deque.removeFirst());
+        assertEquals(3, deque.removeFirst());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void removeLastShouldEmptyDeque() {
+
+        Deque deque = new Deque();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+
+        assertEquals(3, deque.removeLast());
+        assertEquals(2, deque.removeLast());
+        assertEquals(1, deque.removeLast());
+        assertEquals(0, deque.size());
+        assertTrue(deque.isEmpty());
+    }
+
+    @Test
+    public void createsIterator() {
+        Deque<Integer> deque = new Deque();
+        deque.addLast(1);
+        deque.addLast(2);
+        deque.addLast(3);
+
+        for (int i : deque) {
+            System.out.println(i);
+        }
+    }
 
 }
