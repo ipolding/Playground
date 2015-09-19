d18bb0d4773b559d2d5d1b96347dc2c424ad726d
Ian Polding <ipolding@gmail.com>
2015-09-19 23:07:30 +0100

    Subset needs testing

--- Java/Coursera/deque/src/main/java/Subset.java
+++ Java/Coursera/deque/src/main/java/Subset.java
@@ -1,19 +1,21 @@
 import edu.princeton.cs.algs4.StdIn;
 
-/**
- * Created by ian on 19/09/15.
- */
 public class Subset {
 
     public static void main(String[] args) {
 
         //takes a command-line integer k
         Integer k = Integer.parseInt(args[0]);
-
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue();
         //reads in a sequence of N strings from standard input using StdIn.readString();
-        while (!StdIn.isEmpty()) {
+        while (!StdIn.readString().equals("BOO")) {
             String item = StdIn.readString();
+            randomizedQueue.enqueue(item);
         }
         //prints out exactly k of them, uniformly at random
+        for (int i = 0; i<k; k++) {
+            System.out.println(randomizedQueue.dequeue());
+        }
+
     }
 }
