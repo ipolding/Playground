1826d7a2933d107248f719a34e68b9e43fd22181
ian_polding <ian.polding@wowcher.co.uk>
2015-09-21 14:05:36 +0100

    Stub project for collinear assignment

---
+++ Java/Coursera/algorithms-and-data-structures/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ian.polding on 21/09/2015.
+ */
+public class BruteCollinearPoints {
+}
---
+++ Java/Coursera/algorithms-and-data-structures/src/test/java/PointTest.java
@@ -0,0 +1,61 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void testCompareTo_should_compare_points_by_y_coordinates() throws Exception {
+
+    }
+
+    @Test
+    public void testCompareTo_should_break_ties_by_x_coordinates() throws Exception {
+
+    }
+
+    @Test
+    public void testSlopeTo_should_return_the_slope_between_the_invoking_point__x0_y0__and_the_argument_point__x1_y1() throws Exception {
+
+
+    }
+
+    @Test
+    public void test_slopeTo_should_Treat_the_slope_of_a_horizontal_line_segment_as_positive_zero() {
+
+    }
+
+    @Test
+    public void test_slopeTo_should_Treat_the_slope_of_a_vertical_line_segment_as_positive_infinity() {
+
+    }
+
+    @Test
+    public void test_slopeTo_should_treat_the_slope_of_a_degenerate_line_segment_between_a_point_and_itself_as_negative_infinity() {
+
+    }
+
+    @Test
+    public void testSlopeOrder_should_return_a_comparator_that_compares_its_two_argument_points_by_the_slopes_they_make_with_the_invoking_point_x0_y0() throws Exception {
+
+    }
+
+    @Test
+    public void testSlopeOrder_should_Treat_horizontal_line_segments_as_in_the_slopeTo_method() {
+    }
+
+    @Test
+    public void testSlopeOrder_should_Treat_vertical_line_segments_as_in_the_slopeTo_method() {
+
+    }
+
+    @Test
+    public void testSlopeOrder_should_Treat_degenerate_line_segments_as_in_the_slopeTo_method() {
+
+    }
+
+    @Test
+    public void testSlopeOrder() throws Exception {
+
+    }
+}
