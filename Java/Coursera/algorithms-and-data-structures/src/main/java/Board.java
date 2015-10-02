115b8ae13171daa70b4573d358ca0f60dfcf11b4
ian_polding <ian.polding@wowcher.co.uk>
2015-10-02 20:03:24 +0100

    Calculated Hamming distance

--- Java/Coursera/algorithms-and-data-structures/src/main/java/Board.java
+++ Java/Coursera/algorithms-and-data-structures/src/main/java/Board.java
@@ -1,33 +1,130 @@
 // TODO You must use MinPQ for the priority queue(s).
-import edu.princeton.cs.algs4.MinPQ;
 //TODO You may not call any library functions other those in java.lang, java.util, and algs4.jar.
+
 import java.util.*;
 import java.lang.*;
 
 public class Board {
+
+    private static final int BLANK_BLOCK = 0;
+    private final ArrayList<ArrayList<Integer>> columns;
+    private final ArrayList<ArrayList<Integer>> solutionColumns;
+    private final int N;
+
+
     public Board(int[][] blocks) {
+
+        ArrayList<ArrayList<Integer>> columnList = new ArrayList();
+
+        for (int[] inputColumn : blocks) {
+            ArrayList<Integer> column = new ArrayList();
+            int i = 0;
+            for (int block : inputColumn) {
+//                column[i++] = block;
+                column.add(block);
+            }
+            columnList.add(column);
+        }
+        columns = columnList;
+
         // construct a board from an N-by-N array of blocks
         // (where blocks[i][j] = block in row i, column j)
 
         // You may assume that the constructor receives an N-by-N array containing the N2 integers between
         // 0 and N2 − 1, where 0 represents the blank square.
+        N = blocks.length;
+
+        solutionColumns = new ArrayList<ArrayList<Integer>>();
+        int block = 1;
+        for (int i = 0; i < N; i++) {
+            ArrayList<Integer> solvedColumn = new ArrayList();
+            for (int j = 0; j < N; j++) {
+
+                if (block < N * N) {
+                    solvedColumn.add(block++);
+                } else {
+                    solvedColumn.add(BLANK_BLOCK);
+                }
+            }
+            solutionColumns.add(solvedColumn);
 
 
+
+        /*
+        1  2  3
+        4  5  6
+        7  8
+         goal
+         */
+
+        }
+
+        System.out.println(solutionColumns);
+        System.out.println(columns);
     }
 
     //TODO Performance requirements: all Board methods in time proportional to N2 (or better)
 
 
     public int dimension() {// board dimension N
-        return 0;
+        return N;
     }
 
     public int hamming() {// number of blocks out of place
-        return 0;
+        int misplacedBlocks = 0;
+        //TODO  hamming() should return the number of blocks out of position
+        //TODO Recall that the blank square is not considered a block.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                final int solutionBlock = solutionColumns.get(i).get(j);
+                final int currentBlock = columns.get(i).get(j);
+
+
+                if (solutionBlock != currentBlock && 0 != currentBlock) {
+                    misplacedBlocks++;
+                }
+            }
+        }
+        return misplacedBlocks;
     }
 
     public int manhattan() {// sum of manhattan distances between blocks and goal
-        return 0;
+        //TODO  manhattan() should return the sum of the Manhattan distances between the blocks and their goal positions.
+        // TODO Recall that the blank square is not considered a block.
+        int manhattanSums = 0;
+        for (int column = 0; column < N; column++) {
+            //TODO what is the correct column !?
+            for (int row = 0; row < N; row++) {
+                final int currentBlock = columns.get(column).get(row);
+                correctY(currentBlock);
+
+                int rowOffset = Math.abs(row - correctX(currentBlock));
+                int columnOffset = Math.abs(column - correctY(currentBlock));
+
+                //value minus 1 gives array position.
+                final int solutionBlock = solutionColumns.get(column).get(row);
+
+                int currentArrayPosition = currentBlock - 1;
+
+                if (currentBlock != solutionBlock) {
+                    manhattanSums += rowOffset;
+                    manhattanSums += columnOffset;
+                }
+            }
+        }
+        return manhattanSums;
+    }
+
+    //TODO DO NOT SUBMIT AS PUBLIC!!!!!!!!!
+    public int correctY(int currentBlock) {
+//        return (currentBlock / N) - 1;
+        return (int) Math.sqrt(currentBlock)-1;
+    }
+
+    //TODO DO NOT SUBMIT AS PUBLIC!!!!!!!!!
+    public int correctX(int currentBlock) {
+//        return (currentBlock / N) - 1;
+        return (int) Math.sqrt(currentBlock);
     }
 
     public boolean isGoal() {// is this board the goal board?
@@ -40,19 +137,43 @@ public class Board {
 
     @Override
     public boolean equals(Object y) {// does this board equal y?
-        return false;
+
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+
+        return (this.N == that.N && this.columns.equals(that.columns));
+
+//        return (this.month == that.month) && (this.day == that.day) && (this.year == that.year);
+
+
+
+    /*Java has some arcane rules for implementing equals(), discussed on p. 103 of Algorithms, 4th edition. Note that the argument to equals() is required to be Object. */
+
     }
 
     public Iterable<Board> neighbors() {// all neighboring boards
+        /*
+        * Add the items you want to a Stack<Board> or Queue<Board> and return that.
+        * */
         return null;
     }
 
     @Override
     public String toString() {// string representation of this board (in the output format specified below)
-    return null;
+        //TODO implement the toString() method for Board exactly as specified
+        StringBuilder sb = new StringBuilder();
+        sb.append(dimension() + "\n");
+        for (ArrayList<Integer> column : columns) {
+            for (int block : column) {
+                sb.append(String.format("%2d ", block));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {// unit tests (not graded)
     }
-
 }
--- Java/Coursera/algorithms-and-data-structures/src/main/java/BoardTest.java
+++ Java/Coursera/algorithms-and-data-structures/src/main/java/BoardTest.java
@@ -1,4 +1,7 @@
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
 
 public class BoardTest extends TestCase {
 
@@ -14,16 +17,40 @@ public class BoardTest extends TestCase {
 
     }
 
-    public void test_Hamming() {
+    public void test_Hamming_unsolvedPuzzle() {
 //        8  1  3        1  2  3     1  2  3  4  5  6  7  8
 //        4     2        4  5  6     ----------------------
 //        7  6  5        7  8        1  1  0  0  1  1  0  1
 
 //        initial          goal         Hamming = 5 + 0
 
-        Board testObj = new Board(new int[][]{new int[]{}});
+        int[][] blocks = new int[][]{
+                new int[]{8, 1, 3},
+                new int[]{4, 0, 2},
+                new int[]{7, 6, 5}
+        };
+
+        Board testObj = new Board(blocks);
 
         assertEquals(5, testObj.hamming());
+    }
+
+    public void test_Hamming_solvedPuzzle() {
+//        8  1  3        1  2  3     1  2  3  4  5  6  7  8
+//        4     2        4  5  6     ----------------------
+//        7  6  5        7  8        1  1  0  0  1  1  0  1
+
+//        initial          goal         Hamming = 5 + 0
+
+        int[][] solvedBlocks = new int[][]{
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 0}
+        };
+
+        Board testObj = new Board(solvedBlocks);
+
+        assertEquals(0, testObj.hamming());
     }
 
     public void testManhattan() throws Exception {
@@ -32,10 +59,34 @@ public class BoardTest extends TestCase {
 //        7  6  5        7  8        1  2  0  0  2  2  0  3
 
 //        initial          goal        Manhattan = 10 + 0
-        Board testObj = new Board(new int[][]{new int[]{}});
+        int[][] blocks = new int[][]{
+                new int[]{8, 1, 3},
+                new int[]{4, 0, 2},
+                new int[]{7, 6, 5}
+        };
+
+        Board testObj = new Board(blocks);
 
         assertEquals(5, testObj.manhattan());
     }
+
+    public void testManhattan_solvedPuzzle() throws Exception {
+//        8  1  3        1  2  3     1  2  3  4  5  6  7  8
+//        4     2        4  5  6     ----------------------
+//        7  6  5        7  8        1  2  0  0  2  2  0  3
+
+//        initial          goal        Manhattan = 10 + 0
+        int[][] solvedBlocks = new int[][]{
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 0}
+        };
+
+        Board testObj = new Board(solvedBlocks);
+
+        assertEquals(0, testObj.manhattan());
+    }
+
 
     public void testIsGoal() throws Exception {
 
@@ -49,13 +100,109 @@ public class BoardTest extends TestCase {
 
     }
 
+    @Test
+    public void testEquals_isReflexive() throws Exception {
+
+        int[][] blocks = new int[][]{
+                new int[]{0, 1, 2},
+                new int[]{3, 4, 5},
+                new int[]{6, 7, 8}
+        };
+
+        Board testObj = new Board(blocks);
+        assertEquals(testObj, testObj);
+    }
+
+    public void testEquals_isSymmetrical_truePositive() throws Exception {
+        int[][] blocks_1 = new int[][]{
+                new int[]{0, 1, 2},
+                new int[]{3, 4, 5},
+                new int[]{6, 7, 8}
+        };
+
+        Board testObj_1 = new Board(blocks_1);
+        Board testObj_2 = new Board(blocks_1);
+        assertEquals(testObj_1, testObj_2);
+        assertEquals(testObj_2, testObj_1);
+
+    }
+
+
+    public void testEquals_isSymmetrical_trueNegative() throws Exception {
+        int[][] blocks_1 = new int[][]{
+                new int[]{0, 1, 2},
+                new int[]{3, 4, 5},
+                new int[]{6, 7, 8}
+        };
+
+        int[][] blocks_2 = new int[][]{
+                new int[]{0, 6, 2},
+                new int[]{3, 4, 1},
+                new int[]{6, 7, 8}
+        };
+
+        Board testObj_1 = new Board(blocks_1);
+        Board testObj_2 = new Board(blocks_2);
+        assertNotEquals(testObj_1, testObj_2);
+        assertNotEquals(testObj_2, testObj_1);
+
+        }
+
+    public void testEquals_isTransitive() throws Exception {
+
+        int[][] blocks_1 = new int[][]{
+                new int[]{0, 1, 2},
+                new int[]{3, 4, 5},
+                new int[]{6, 7, 8}
+        };
+
+        Board testObj_1 = new Board(blocks_1);
+        Board testObj_2 = new Board(blocks_1);
+        Board testObj_3 = new Board(blocks_1);
+        assertEquals(testObj_1, testObj_2);
+        assertEquals(testObj_2, testObj_3);
+        assertEquals(testObj_1, testObj_3);
+    }
+
     public void testNeighbors() throws Exception {
 
     }
 
     public void testToString() throws Exception {
+        int[][] blocks = new int[][]{
+                new int[]{1, 0, 3},
+                new int[]{4, 2, 5},
+                new int[]{7, 8, 6}
+        };
+
+        Board testObj = new Board(blocks);
+
+        System.out.println(testObj);
+        assertEquals("3\n 1  0  3 \n 4  2  5 \n 7  8  6 \n", testObj.toString());
 
     }
+
+
+    @Test
+    public void test_correctRow() {
+
+//        8  1  3        1  2  3     1  2  3  4  5  6  7  8
+//        4     2        4  5  6     ----------------------
+//        7  6  5        7  8        1  1  0  0  1  1  0  1
+
+//        initial          goal         Hamming = 5 + 0
+
+            int[][] blocks = new int[][]{
+                    new int[]{8, 1, 3},
+                    new int[]{4, 0, 2},
+                    new int[]{7, 6, 5}
+            };
+
+            Board testObj = new Board(blocks);
+
+            assertEquals(2, testObj.correctX(8));
+            assertEquals(3, testObj.correctY(8));
+        }
 
     public void testMain() throws Exception {
 
