9eec4460b1b1b68c92c7738bc0922b0559add484
Ian Polding <ipolding@gmail.com>
2015-10-22 23:50:37 +0100

    Get top tracks for a single artist

--- Scala/spotify-playlister/app/controllers/Application.scala
+++ Scala/spotify-playlister/app/controllers/Application.scala
@@ -4,18 +4,23 @@ import com.google.inject.Inject
 import play.api._
 import play.api.libs.ws.WSClient
 import play.api.mvc._
-import spotify.Playlister
-import scala.concurrent.ExecutionContext.Implicits.global
+import spotify.{Artist, Playlister}
 
 class Application @Inject() (ws: WSClient)  extends Controller {
-
+//Radiohead ID = 4Z8W4fKeB5YxbusRsdQVPb
   def index = Action {
 
     import scala.concurrent.ExecutionContext.Implicits.global
 
     val playlist = new Playlister(ws)
-    playlist.searchForArtist("radiohead")
+    playlist
+      .searchForArtist("radiohead")
       .onComplete(artist => print(artist))
+
+    playlist
+      .getTopTracks(Artist("Radiohead", "4Z8W4fKeB5YxbusRsdQVPb"))
+      .onComplete(print(_))
+
     Ok(views.html.index("Your new application is ready."))
   }
 
--- Scala/spotify-playlister/app/spotify/Playlister.scala
+++ Scala/spotify-playlister/app/spotify/Playlister.scala
@@ -1,44 +1,53 @@
 package spotify
 
 import com.google.inject.Inject
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsString, JsObject}
 import play.api.libs.ws.WSClient
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-case class Track(artist: String, name: String, url : String)
+
+case class Track(spotifyUri: String)
 case class Artist(name: String, spotifyId: String)
 
 class Playlister @Inject() (ws: WSClient) {
-
-  def generatePlaylist(artistNames :  List[String]) : List[Track] = {
-      List()
-  }
-
-  def searchForArtists(artistNames : List[String]) : List[Artist] = {
-    List()
-  }
+//
+//  def generatePlaylist(artistNames :  List[String]) : Future[List[Track]] = {
+//    Future(List())
+//  }
+//
+//  def searchForArtists(artistNames : List[String]) : List[Future[Artist]] = {
+//    List()
+//  }
 
   def searchForArtist(artistName : String) : Future[Artist] = {
-    val futureResponse = ws.url("https://api.spotify.com/search").withQueryString(
+    val futureResponse = ws.url("https://api.spotify.com/v1/search").withQueryString(
       "type" -> "artist",
+      "q" -> artistName,
       "limit" -> "1").get()
     futureResponse.map(response => {
       val jsObject = response.json.asInstanceOf[JsObject]
-      print(jsObject)
-      Artist("name", "id")
+      val id = jsObject \\ "id"
+      val name = jsObject \\ "name"
+      Artist(name.head.asInstanceOf[JsString].toString, id.head.asInstanceOf[JsString].toString)
     })
   }
 
-  def getTopTracks(artist : Artist) : List[Track] = {
-    List()
+  def getTopTracks(artist : Artist) : Future[List[Track]] = {
+    val futureResponse = ws.url(s"https://api.spotify.com/v1/artists/${artist.spotifyId}/top-tracks")
+        .withQueryString(
+            "country" -> "GB"
+        ).get()
+    futureResponse.map(response =>  {
+      val jsonResponse = response.json.asInstanceOf[JsObject]
+      transformToTracks(jsonResponse)
+    }
+    )
   }
 
-  def transformToTrack (apiResponse : JsObject ) : Track = {
-    Track("artist", "name", "url")
+  private def transformToTracks (apiResponse : JsObject ) : List[Track] = {
+    val uris = (apiResponse \\ "uri").map(_.as[String])
+    val tracks = uris.filter(_.contains("track")).map(Track(_)).toList
+    tracks
   }
-
-
-  //flatten List[List[Track]
-
-
 }
