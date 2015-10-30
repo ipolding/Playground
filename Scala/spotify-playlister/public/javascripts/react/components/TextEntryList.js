0d86694683c5fd9326e06c155f5aa462e8aa9fb4
ian_polding <ian.polding@wowcher.co.uk>
2015-10-30 14:02:02 +0000

    Remove separate form Component from UI

--- Scala/spotify-playlister/public/javascripts/react/components/TextEntryList.js
+++ Scala/spotify-playlister/public/javascripts/react/components/TextEntryList.js
@@ -1,7 +1,22 @@
 var TextEntry = React.createClass({
+
+  componentDidMount: function() {console.log("Text entry mounted")},
+
+  keyHasBeenPressed : function(e) {
+      if (e.keyCode == 13) {
+          this.props.handleEdit({entry: "TODO get this elements value"});
+      }      
+  },
+
+  editHasHappened : function(e) {
+      console.log("attempt to get value " + this.refs);
+
+      this.props.handleEdit({entry: "TODO get this elements value"});   
+  },
+
   render: function() {
     return (
-      <div className="textEntry">
+      <div onBlur={this.editHasHappened} onKeyDown={this.keyHasBeenPressed} className="textEntry">
         <input defaultValue={this.props.entry}></input>          
           {this.props.children}
       </div>
@@ -10,10 +25,14 @@ var TextEntry = React.createClass({
 });
 
 var PreviousTextEntries = React.createClass({
+    componentDidMount: function() {console.log("Previous text entries mounted")},
+
+
   render: function() {
+    var handleEdit = this.props.handleEdit;
     var textEntryNodes = this.props.data.map(function (textEntry) {
       return (
-        <TextEntry entry={textEntry.entry}>
+        <TextEntry handleEdit={handleEdit} entry={textEntry.entry}>
           {textEntry.text}
         </TextEntry>
       );
@@ -26,68 +45,60 @@ var PreviousTextEntries = React.createClass({
   }
 });
 
-var TextEntryForm = React.createClass({
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var entry = this.refs.entry.value.trim();
-    if (!entry) {
-      return;
-    }
-    this.props.onTextEntrySubmit({entry: entry});
-    this.refs.entry.value = '';    
-    return;
-  },
-  render: function() {
-    return (
-      <form className="textEntryForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Artist" ref="entry" />        
-      </form>
-    );
-  }
-});
+// var TextEntryForm = React.createClass({
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var entry = this.refs.entry.value.trim();
+//     if (!entry) {
+//       return;
+//     }
+//     this.props.onTextEntrySubmit({entry: entry});
+//     this.refs.entry.value = '';    
+//     return;
+//   },
+//   render: function() {
+//     return (
+//       <form className="textEntryForm" onSubmit={this.handleSubmit}>
+//         <input type="text" placeholder="Artist" ref="entry" />        
+//       </form>
+//     );
+//   }
+// });
 
 var TextEntryBox = React.createClass({
-  handleTextEntrySubmit: function(textEntry) {
-    var textEntries = this.state.data;
-    var newTextEntrys = textEntries.concat([textEntry]);
-    this.setState({data: newTextEntrys});
-    this.toJson();
-  },
-  getInitialState: function() {
-    return {data: []};
-  },
   render: function() {
     return (
       <div className="textEntryBox">
         <h1>Artists</h1>
-        <PreviousTextEntries data={this.state.data} />
-        <TextEntryForm onTextEntrySubmit={this.handleTextEntrySubmit} />
+        <PreviousTextEntries handleEdit={this.props.handleTextEntryEdit} data={this.props.data} />
+        <TextEntry handleEdit={this.props.handleTextEntryEdit} />
       </div>
     );
-  },
-
-  toJson : function() {
-    var stringArray = this.state.data.map(getTextEntryValues)
-    return stringArray;
-   }
+  }
 });
 
 var PlayListSubmitter = React.createClass({
-  handleTextEntrySubmit: function(textEntry) {
+  handleTextEntryEdit: function(textEntry) {
+    console.log("handling text entry submit")
     var textEntries = this.state.data;
     var newTextEntrys = textEntries.concat([textEntry]);
-    this.setState({data: newTextEntrys});
-    this.toJson();
+    this.setState({data: newTextEntrys});     
   },
+
   getInitialState: function() {
     return {data: []};
   },
+
+  getPlaylist : function() {
+    
+    console.log("Sending: " + this.toJson())
+  },
+
   render: function() {
     return (
       <div className="textEntryBox">
-        <TextEntryBox />
-        // Parent can call child method from here
-        <button>HELLO WORLD</button>
+        <TextEntryBox handleTextEntryEdit={this.handleTextEntryEdit} data={this.state.data}/>
+        <button onClick={this.getPlaylist} className="btn btn-primary btn-lg">Get Top Tracks! &raquo;</button>
       </div>
     );
   },
