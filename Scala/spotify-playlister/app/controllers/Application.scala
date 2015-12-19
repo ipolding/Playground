1ae183a6144cced7212445cf5b4039efa4e9da1f
Ian Polding <ipolding@gmail.com>
2015-12-19 22:44:30 +0000

    Log error when handling. Convert query String to list in Controller

--- Scala/spotify-playlister/app/controllers/Application.scala
+++ Scala/spotify-playlister/app/controllers/Application.scala
@@ -1,21 +1,32 @@
 package controllers
 
 import com.google.inject.Inject
-import play.api._
+import play.api.libs.json.{JsArray, Json}
 import play.api.libs.ws.WSClient
 import play.api.mvc._
+import play.api.Logger
 import scala.concurrent.Future
 import spotify.{Artist, Playlister}
 
 class Application @Inject() (ws: WSClient)  extends Controller {
 //Radiohead ID = 4Z8W4fKeB5YxbusRsdQVPb
-  def playlist(artistQuery : List[String]) = Action.async {
+  def playlist(artistQuery : String) = Action.async {
 
     import scala.concurrent.ExecutionContext.Implicits.global
 
+    val artistList = artistQuery.split(",");
+
+    Logger.info(s"searched for $artistQuery");
+
     val playlist = new Playlister(ws)
-    val futureTopTracks = playlist.getArtistsTopTracks(artistQuery)
+    val futureTopTracks = playlist.getArtistsTopTracks(artistList.toList)
     futureTopTracks.map{
-        topTracks => Ok(views.html.index(topTracks))}
+        topTracks => {
+          val ids: List[String] = topTracks.map(_.spotifyId)
+          val jsonResponse  = Json.obj("playlist" -> Json.toJson(ids))
+          Ok(jsonResponse)
+        } 
+    }
+     .recover({case err : Throwable => Logger.error("Error!", err); BadRequest(JsArray())})
   }
 }
--- Scala/spotify-playlister/app/spotify/Playlister.scala
+++ Scala/spotify-playlister/app/spotify/Playlister.scala
@@ -2,6 +2,7 @@ package spotify
 
 import com.google.inject.Inject
 import play.api.libs.json.{JsString, JsObject}
+import play.api.Logger
 import play.api.libs.ws.WSClient
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -30,25 +31,42 @@ class Playlister @Inject() (ws: WSClient) {
       "q" -> artistName,
       "limit" -> "1").get()
     futureResponse.map(response => {
+      Logger.info(s"artistResponse=${response.body}")
+
       val jsObject = response.json.asInstanceOf[JsObject]
+      
       val id = jsObject \\ "id"
       val name = jsObject \\ "name"
-      Artist(name.head.asInstanceOf[JsString].toString, id.head.asInstanceOf[JsString].toString)
+
+      val artistName =  name.headOption.getOrElse(JsString("")).asInstanceOf[JsString].value
+      val artistId =    id.headOption.getOrElse(JsString("")).asInstanceOf[JsString].value
+      
+      Artist(artistName, artistId)
     })
   }
 
   def getTopTracks(artist : Artist) : Future[List[Track]] = {
-    val futureResponse = ws.url(s"https://api.spotify.com/v1/artists/${artist.spotifyId}/top-tracks")
+
+    if (artist.name.isEmpty) {
+      return Future(List())
+    }
+
+    val request = ws.url(s"https://api.spotify.com/v1/artists/${artist.spotifyId}/top-tracks")
         .withQueryString(
             "country" -> "GB"
-        ).get()
-    futureResponse.map(response =>  {
+        )
+    val futureResponse = request.get()
+      futureResponse.map(response =>  {
+      if (response.status != 200) {Logger.error(s"${request.url} gave a ${response.status} with ${response.body}")}
+      Logger.info(s"topTracksResponse=${response.body}")
+
       val jsonResponse = response.json.asInstanceOf[JsObject]
       transformToTracks(jsonResponse)
     })
   }
 
   private def transformToTracks (apiResponse : JsObject ) : List[Track] = {
+    Logger.debug(s"trackResponse=$apiResponse")
     val tracksJson = (apiResponse \ "tracks").as[List[JsObject]]
     val trackIds : List[String] = tracksJson.map{json => (json \ "id").as[String]}
     val tracks = trackIds.map(Track(_)).toList
